5f311cb412572794516364913c50e0ecd9dc6163
teviw000 <teviw000@tamu.edu>
2019-10-17 08:30:20 -0500

    updating dummy db

--- review/db/seeds.rb
+++ review/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+records = [	{:PoI => 'McDonalds', :rating => '5', :cost => '1', phoneNum => 1112223333, :city => 'Austin', :state => 'Texas'},
+		{:PoI => 'Zachary', :rating => '1', :cost => '2', phoneNum => 3332221111, :city => 'College Station', :state => 'Texas'},
+		{:PoI => 'Wendys', :rating => '3', :cost => '2', phoneNum => 1112223344, :city => 'Bryan', :state => 'Texas'},
+	]
+
+records.each do |record|
+	Record.create!(record)
+end
