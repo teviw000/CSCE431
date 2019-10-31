5e047940e0e63f88c01a3f18ce4480f3bca72c3c
Zach Hein <zach.hein@tamu.edu>
2019-10-31 10:51:40 -0500

    working on text field

--- reviews/app/controllers/reviews_controller.rb
+++ reviews/app/controllers/reviews_controller.rb
@@ -12,16 +12,8 @@ class ReviewsController < ApplicationController
   def create
     @category = Category.new(category_params)
 
-    respond_to do |format|
-      if @category.save
-        format.html { redirect_to @category, notice: 'Category was successfully created.' }
-        format.json { render :show, status: :created, location: @category }
-        format.js { render js: 'window.top.location.reload(true);' }
-      else
-        format.html { render :new }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
-    end
+    if @category.save
+      redirect_to :action => "http://localhost:3000"
   end
 
 end
