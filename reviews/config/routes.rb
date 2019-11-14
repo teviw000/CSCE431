ad07c1d9db0cd422fd720b9ea65c22335c9f33ff
rmickus <rmickus@tamu.edu>
2019-11-13 22:51:14 -0600

    moving new to leave_review

--- reviews/config/routes.rb
+++ reviews/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'reviews/index'
-  get 'reviews/new'
+  get 'reviews/leave_review/:id', to: 'reviews#leave_review', as: 'leave_review'
   get 'reviews/emergency'
   resources :reviews
   root 'reviews#index'
