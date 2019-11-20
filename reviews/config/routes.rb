826e727a028e28408a679b13cd7f95fa73f8e9f4
tylooor <tylooor@tamu.edu>
2019-11-19 22:54:09 -0600

    can store user data in DB

--- reviews/config/routes.rb
+++ reviews/config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   get 'reviews/leave_review/:id', to: 'reviews#leave_review', as: 'leave_review'
   get 'sessions/create'
   get 'reviews/emergency'
-  get '/login', to: redirect('/auth/google_oauth2')
-  get '/auth/google_oauth2/callback', to: 'sessions#create'
+  # get '/login', to: redirect('/auth/google_oauth2')
+  devise_for :users, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
   resources :reviews
   root 'reviews#index'
 end
--- reviews/db/schema.rb
+++ reviews/db/schema.rb
@@ -10,21 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_19_042421) do
+ActiveRecord::Schema.define(version: 2019_11_20_042407) do
 
-  create_table "reviews", force: :cascade do |t|
-    t.string "name"
-    t.string "city"
-    t.string "country"
-    t.integer "review"
-    t.integer "cost"
-    t.float "latitude"
-    t.float "longitude"
-    t.text "description"
-    t.text "tags"
-    t.string "phone"
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
