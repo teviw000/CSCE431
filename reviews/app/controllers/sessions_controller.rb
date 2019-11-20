107b3c483d886a65e20802541066aacbd55bbb46
tylooor <tylooor@tamu.edu>
2019-11-20 01:11:59 -0600

    can log in, but can't store user in session

--- reviews/app/controllers/sessions_controller.rb
+++ reviews/app/controllers/sessions_controller.rb
@@ -1,6 +1,19 @@
 class SessionsController < ApplicationController
-  def create
-  end
-  def destory
-  end
+    def create
+        user_info = request.env["omniauth.auth"]
+
+        user           = User.new
+        user.id        = user_info["uid"]
+        user.name      = user_info["info"]["name"]
+        user.email     = user_info["email"]
+        # user.image_url = user_info["info"]["image"]
+    
+        session[:user] = Marshal.dump user
+    
+        redirect_to root_path
+    end
+    def destroy
+        session.delete :user
+        redirect_to root_path
+    end
 end
--- reviews/config/routes.rb
+++ reviews/config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   get 'reviews/leave_review/:id', to: 'reviews#leave_review', as: 'leave_review'
   get 'sessions/create'
   get 'reviews/emergency'
-  # get '/login', to: redirect('/auth/google_oauth2')
-  devise_for :users, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
+  get "/login", to: redirect("/auth/google_oauth2")
+  get "/auth/google_oauth2/callback", to: "sessions#create"
+  get "/logout", to: "sessions#destroy"
+  resource :session, only: [:create, :destroy]
   resources :reviews
   root 'reviews#index'
 end
