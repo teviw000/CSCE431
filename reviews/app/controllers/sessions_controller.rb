0ad104ddf68fafd8991f7ba94013d313e8bdd6ec
tylooor <tylooor@tamu.edu>
2019-11-21 01:15:50 -0600

    login and styling are done for the menu

--- reviews/app/controllers/sessions_controller.rb
+++ reviews/app/controllers/sessions_controller.rb
@@ -4,11 +4,15 @@ class SessionsController < ApplicationController
 
         user           = User.new
         user.id        = user_info["uid"]
-        user.name      = user_info["name"]
-        user.email     = user_info["email"]
+        user.name      = user_info["info"]["name"]
+        user.email     = user_info["info"]["email"]
         # user.image_url = user_info["info"]["image"]
     
         session[:user] = Marshal.dump user
+        puts session[:user]
+        puts user_info["uid"]
+        puts user_info["info"]["name"]
+        puts user_info["info"]["email"]
     
         redirect_to root_path
     end
--- reviews/app/models/user.rb
+++ reviews/app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
     attr_accessor :id, :name, :email
 
     def marshal_dump
-        [:id, :name, :email]
+        [@id, @name, @email]
     end
     def marshal_load array
-        id, name, email = array
+        @id, @name, @email = array
     end
 end
