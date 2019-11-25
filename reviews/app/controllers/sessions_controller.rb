1d4289af5435b343271d6392d34596db5320b7fe
tylooor <tylooor@tamu.edu>
2019-11-25 15:48:36 -0600

    heroku

--- reviews/app/controllers/sessions_controller.rb
+++ reviews/app/controllers/sessions_controller.rb
@@ -2,17 +2,17 @@ class SessionsController < ApplicationController
     def create
         user_info = request.env["omniauth.auth"]
 
-        user           = User.new
+        # user           = User.new
         # user.id        = user_info["uid"]
         # user.name      = user_info["info"]["name"]
         # user.email     = user_info["info"]["email"]
         # # user.image_url = user_info["info"]["image"]
     
-        session[:user] = Marshal.dump user
+        # session[:user] = Marshal.dump user
         # puts session[:user]
-        # puts user_info["uid"]
-        # puts user_info["info"]["name"]
-        # puts user_info["info"]["email"]
+        puts user_info["uid"]
+        puts user_info["info"]["name"]
+        puts user_info["info"]["email"]
     
         redirect_to root_path
     end
