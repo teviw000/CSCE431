f16c9484b263994c8a88300b4a3ddf5f314eba3c
tylooor <tylooor@tamu.edu>
2019-11-25 14:52:38 -0600

    commenting for debugging

--- reviews/app/controllers/sessions_controller.rb
+++ reviews/app/controllers/sessions_controller.rb
@@ -2,13 +2,13 @@ class SessionsController < ApplicationController
     def create
         user_info = request.env["omniauth.auth"]
 
-        # user           = User.new
-        # user.id        = user_info["uid"]
-        # user.name      = user_info["info"]["name"]
+        user           = User.new
+        user.id        = user_info["uid"]
+        user.name      = user_info["info"]["name"]
         # user.email     = user_info["info"]["email"]
         # # user.image_url = user_info["info"]["image"]
     
-        # session[:user] = Marshal.dump user
+        session[:user] = Marshal.dump user
         # puts session[:user]
         # puts user_info["uid"]
         # puts user_info["info"]["name"]
