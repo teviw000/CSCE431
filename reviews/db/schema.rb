de51bd09abd2cfd7f68cd54d55384074db2931c3
zach-hein <zach.hein@tamu.edu>
2019-12-02 11:12:33 -0600

    merged master into branch

--- reviews/db/schema.rb
+++ reviews/db/schema.rb
@@ -30,9 +30,6 @@ ActiveRecord::Schema.define(version: 2019_11_21_233128) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "uid"
-    t.string "name"
-    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
